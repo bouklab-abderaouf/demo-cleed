bb1cf3056fa3051412b6b8681dcdf97cd7e0d89f
Raouf <abderaouf-hocine.bouklab@epitech.eu>
2023-08-26 17:49:44 +0200

    init for deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="className=' w-full h-auto flex justify-start sm:justify-between flex-col-reverse sm:flex-row sm:mt-10 mt-10'>">
+      <div className="relative w-full h-[1000px] md:h-[1000px]">
+            <iframe
+              src="https://bouklababdelraouf.bubbleapps.io/version-test"
+              name="bubble-app"
+              title="Bubble App"
+              scrolling="no"
+              frameBorder="0"
+              allowFullScreen
+              className="absolute inset-0 w-full h-full border-none"
+            ></iframe>
+          </div>
     </div>
   );
 }
