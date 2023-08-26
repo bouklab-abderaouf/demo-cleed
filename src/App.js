39e15c40d5bfd54c3170d847eb7dba54a083804d
Raouf <abderaouf-hocine.bouklab@epitech.eu>
2023-08-26 17:54:15 +0200

    title add

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="className=' w-full h-auto flex justify-start sm:justify-between flex-col-reverse sm:flex-row sm:mt-10 mt-10'>">
-      <div className="relative w-full h-[1000px] md:h-[1000px]">
+    <div className="className=' w-full h-auto flex-col justify-start sm:justify-between flex-col-reverse sm:flex-row sm:mt-10 mt-10'> ">
+    <h1 className="text-3xl font-bold text-center">embedded Test</h1>
+      <div className="relative w-full h-[1000px] md:h-[1000px] ">
             <iframe
               src="https://bouklababdelraouf.bubbleapps.io/version-test"
               name="bubble-app"
